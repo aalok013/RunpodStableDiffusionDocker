8c0d19961fcbf79472efebc2710d5033db7d9865
aalok013 <108550064+aalok013@users.noreply.github.com>
2023-08-07 15:10:09 +0530

    Update model-download.sh

--- DockerFiles/model-download.sh
+++ DockerFiles/model-download.sh
@@ -1,11 +1,13 @@
 wget -O /workspace/stable-diffusion-webui/models/Stable-diffusion/v1.5.safetensors https://huggingface.co/runwayml/stable-diffusion-v1-5/resolve/main/v1-5-pruned.safetensors
 
-wget -O /workspace/stable-diffusion-webui/models/Stable-diffusion/dream.safetensors https://civitai.com/api/download/models/94081
+#wget -O /workspace/stable-diffusion-webui/models/Stable-diffusion/dream.safetensors https://civitai.com/api/download/models/94081
 
-wget -O /workspace/stable-diffusion-webui/models/Stable-diffusion/Deliberate.safetensors https://civitai.com/api/download/models/15236
+#wget -O /workspace/stable-diffusion-webui/models/Stable-diffusion/Deliberate.safetensors https://civitai.com/api/download/models/15236
 
 wget -O /workspace/stable-diffusion-webui/models/Stable-diffusion/Reliberate.safetensors https://civitai.com/api/download/models/84576
 
 wget -O /workspace/stable-diffusion-webui/models/Stable-diffusion/Hentai.safetensors https://civitai.com/api/download/models/62528
 
 wget -O /workspace/stable-diffusion-webui/models/Lora/details.safetensors https://civitai.com/api/download/models/87153
+
+wget https://huggingface.co/lllyasviel/ControlNet-v1-1/resolve/main/control_v11p_sd15_canny.pth -O /workspace/stable-diffusion-webui/models/Lora/details.safetensors
